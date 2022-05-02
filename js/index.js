fab99ecbf5245cfb3de0281a88c4452fbaa19f00
Noothowl <martinchipoco@gmail.com>
2022-05-02 16:46:46 -0400

    Table + JS Btn + Logo

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.querySelector("#btn-register").addEventListener("click", ()=>{
+    let nombre = document.querySelector("#WarriorName-txt").value;
+
+    alert("Doxxeao de pana");
+    console.log("Hola chanchito feliz", nombre);
+});
