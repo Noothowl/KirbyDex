e67ffb28bbe1acb41b8a45e72c188c8fbfc394ee
Noothowl <martinchipoco@gmail.com>
2022-05-02 16:52:13 -0400

    Alert change

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 document.querySelector("#btn-register").addEventListener("click", ()=>{
     let nombre = document.querySelector("#WarriorName-txt").value;
 
-    alert("Intentaste registrar a: "+nombre);
+    alert("Intentaste registrar a "+nombre);
     console.log("Hola chanchito feliz", nombre);
 });
