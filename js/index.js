b9ea6d1105ded1afb77f1f665bf40d7e66151db0
Noothowl <martinchipoco@gmail.com>
2022-05-09 12:09:15 -0400

    Creación de objeto por js + SweetAlert

--- js/index.js
+++ js/index.js
@@ -14,17 +14,63 @@ tinymce.init({
     'removeformat | help',
     content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
   });
-  
-  
+  //Funciones se definen arriba
+  //cargarTabla
+  const cargarTabla = ()=>{
+    // Generación dinámica
+      //1 Ref a la tabla
+      let tbody = document.querySelector("#tbody-table");
+      //2 Recorrer lista de warriors
+      for(let i=0; i<warriors.length; i++){
+        //3 por cada warrior generar tr
+        let tr = document.createElement("tr");     //dentro del parentesis puede ir cualquier elemento html
+        //4 por cada atributo generar td
+        let tdNro = document.createElement("td");
+        let tdNombre = document.createElement("td");
+        let tdElemento = document.createElement("td");
+        let tdDescripcion = document.createElement("td");
+        let tdAcciones = document.createElement("td");
+        //5 agregar td a los tr     *appendChild para agregar elemento dentro de otro
+        tr.appendChild(tdNro);
+        tr.appendChild(tdNombre);
+        tr.appendChild(tdElemento);
+        tr.appendChild(tdDescripcion);
+        tr.appendChild(tdAcciones);
+        //6 agregar tr a la tabla
+        tbody.appendChild(tr);
+      }
+
+
+    
+  };
+  //Crea un arreglo --> en verdad es una "lista" porque es dinámica
+  const warriors = [];
+
 document.querySelector("#btn-register").addEventListener("click", ()=>{
     let name = document.querySelector("#WarriorName-txt").value;         //value is for value of "input"
+
+    let type = document.querySelector("#Type-select").value;
 
     let description = tinymce.get("WarriorDescription-txt").getContent();//tinyme.get - g.etContent is for value of "tinymc"
 
     let legend = document.querySelector("#Legend-toggle-yes").checked;    //checked is for value of "radio button"
 
-    let type = document.querySelector("#Type-select").value;
+    
+    
+    //Crea el objeto "warrior"
+    let warrior= {};
+    //Crea la propiedad de warrior
+    warrior.name =name;
+    warrior.description = description;
+    warrior.legend= legend;
+    warrior.type = type;
+    //Añadir objeto a la lista
+    warriors.push(warrior);
+    //Cargar elemento en la tabla
+    cargarTabla();
 
-    console.log("RegisterIntentaste registrar a ", name,description,legend,type);
+    //SweetAlert
+    Swal.fire("Registro exitoso!","EL guerrero ingresado", "success");
+
 
 });
